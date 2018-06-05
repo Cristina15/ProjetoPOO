42b38e9095f8e6a4a914044a76e3f9e8ec810e77
matheus.jmaia <matheus.jmaia@cas493929w10b22.senacsp.edu.br>
2018-06-05 19:18:11 -0300

    520

--- src/views/regar/RegarArbusto.java
+++ src/views/regar/RegarArbusto.java
@@ -22,7 +22,8 @@ public class RegarArbusto extends javax.swing.JInternalFrame {
     public RegarArbusto() {
         initComponents();
     }
-
+    
+     resultSearch = refreshList();
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
